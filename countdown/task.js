0f06f2bc58e5739d49dca8235758780386f558bb
Iliakra <51176758+Iliakra@users.noreply.github.com>
2019-10-23 13:09:59 +0300

    Delete task.js

--- countdown/task.js
+++
@@ -1,9 +0,0 @@
-let startTime = document.getElementById('timer');
-let totalTime = startTime.textContent;
-function funcTimer(totalTime) {
-		totalTime = totalTime - 1;
-		return totalTime
-};
-totalTime = setInterval(funcTimer, 1000);
-console.log(totalTime);
-
