963efb0c23a9032ed55437f62d2053a36f2b70cc
Iliakra <51176758+Iliakra@users.noreply.github.com>
2019-11-06 22:37:25 +0300

    Add files via upload

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,20 +1,69 @@
-let menu = document.querySelectorAll('ul.menu');
-let menuLink = menu[0].querySelectorAll('a.menu__link');
-let menuSub = menu[0].querySelectorAll('ul.menu_sub');
-let arrMenu = Array.from(menuLink);
-let arrSub = Array.from(menuSub);
-console.log(arrMenu[6]);
-arrMenu[1].onclick = function () {
-    arrSub[0].classList.add('menu_active');
-    arrSub[1].classList.remove('menu_active');
+let menu = document.querySelectorAll('ul.menu_main > li > a');
+let arrFromMenu = Array.from(menu);
+let menuSub = document.querySelectorAll('ul.menu_sub');
+let arrMenuSub = Array.from(menuSub);
+
+arrFromMenu[0].onclick = function() {
+	let x = arrFromMenu[0].closest('.menu__item');
+	let y = x.querySelector('ul.menu_sub');
+    if (y.className.includes('menu_active')) {
+		y.classList.remove('menu_active');
+    } else {
+    	for (i=0; i < arrMenuSub.length; i++) {
+    	if (arrMenuSub[i].className.includes('menu_active')) {
+    		arrMenuSub[i].classList.remove('menu_active');
+    	}
+    }
+    	
+		y.classList.add('menu_active')
+    }
     return false
 }
-arrMenu[5].onclick = function () {
-    arrSub[1].classList.add('menu_active');
-    arrSub[0].classList.remove('menu_active');
+
+arrFromMenu[1].onclick = function() {
+	let x = arrFromMenu[1].closest('.menu__item');
+	let y = x.querySelector('ul.menu_sub');
+	if (y.className.includes('menu_active')) {
+		y.classList.remove('menu_active');
+    } else {
+    	for (i=0; i < arrMenuSub.length; i++) {
+    	if (arrMenuSub[i].className.includes('menu_active')) {
+    		arrMenuSub[i].classList.remove('menu_active');
+    	}
+    }
+    	y.classList.add('menu_active');
+    }
     return false
 }
 
+arrFromMenu[2].onclick = function() {
+	let x = arrFromMenu[2].closest('.menu__item');
+	let y = x.querySelector('ul.menu_sub');
+	if (y.className.includes('menu_active')) {
+		y.classList.remove('menu_active');
+    } else {
+		for (i=0; i < arrMenuSub.length; i++) {
+    	if (arrMenuSub[i].className.includes('menu_active')) {
+    		arrMenuSub[i].classList.remove('menu_active');
+    	}
+    }
+    	y.classList.add('menu_active');
+    }
+    return false
+}
 
-
-
+arrFromMenu[3].onclick = function() {
+	let x = arrFromMenu[3].closest('.menu__item');
+	let y = x.querySelector('ul.menu_sub');
+	if (y.className.includes('menu_active')) {
+		y.classList.remove('menu_active');
+    } else {
+    	for (i=0; i < arrMenuSub.length; i++) {
+    	if (arrMenuSub[i].className.includes('menu_active')) {
+    		arrMenuSub[i].classList.remove('menu_active');
+    	}
+    }
+    	y.classList.add('menu_active');
+    }
+    return false
+}
